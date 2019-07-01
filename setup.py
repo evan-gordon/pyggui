2fb3d6d708db2155d5069d3dcace80ef139ed2a6
evan-gordon <evanwgordon@yahoo.com>
2019-07-01 01:04:41 -0700

    feat: added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='pyggui',
+    version='0.1',
+    packages=[],
+    license=
+    'Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
